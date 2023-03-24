5c9b36aee02770868d85a4ad6a1f9f8606c81eba
siposm <commit@siposm.hu>
2023-03-24 07:05:17 +0100

    refactored prop names

--- modul-6/worker-manager_v2/student.js
+++ modul-6/worker-manager_v2/student.js
@@ -1,11 +1,12 @@
 export class Student {
-    constructor(NeptunID, Name, IsActive, BirthYear, CompletedCredits, ActiveSemesterCount, Image) {
-        this.NeptunID = NeptunID
-        this.Name = Name
-        this.IsActive = IsActive
-        this.BirthYear = BirthYear
-        this.CompletedCredits = CompletedCredits
-        this.ActiveSemesterCount = ActiveSemesterCount
-        this.Image = Image
+    constructor(id, name, isActive, birthYear, connections, completedCredits, activeSemesterCount, image) {
+        this.id = id
+        this.name = name
+        this.isActive = isActive
+        this.birthYear = birthYear
+        this.connections = connections
+        this.completedCredits = completedCredits
+        this.activeSemesterCount = activeSemesterCount
+        this.image = image
     }
 }
