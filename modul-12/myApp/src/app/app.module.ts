fddd69685e114fae8764ffcdd6c7e6505e180704
siposm <commit@siposm.hu>
2023-05-03 10:15:53 +0200

    footer updated

--- modul-12/myApp/src/app/app.module.ts
+++ modul-12/myApp/src/app/app.module.ts
@@ -8,16 +8,26 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatButtonModule } from '@angular/material/button';
+import { MatDividerModule } from '@angular/material/divider';
+import {MatIconModule} from '@angular/material/icon';
+
+import { NavigationComponent } from './navigation/navigation.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MatSlideToggleModule,
+    MatDividerModule,
+    MatIconModule,
+
     MatMenuModule,
     MatButtonModule,
   ],
